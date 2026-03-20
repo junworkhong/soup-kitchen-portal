7efdc4b4e65368b5b9b8b4f0abec235f855d1ef9
junworkhong <junworkhong@gmail.com>
2026-03-19 23:11:26 -0400

    Announcement schema

--- backend/models/Announcement.js
+++ backend/models/Announcement.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const announceSchema = new mongoose.Schema({
     title: { type: String, required: true },
     body: { type: String, required: true },
-    author: { type: String, required: true },
+    author: { type: String, required: false },
 }, { timestamps: true }); // adds createdAt and updatedAt automatically
 
 module.exports = mongoose.model('Announcement', announceSchema);
